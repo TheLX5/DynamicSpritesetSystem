0ea804bf4759828959df7fe079517b7346d8cd59
TheLX5 <luisyuregi@gmail.com>
2024-02-05 16:46:30 -0700

    Remove read3()

--- uberasm/gamemode/dss_prepare.asm
+++ uberasm/gamemode/dss_prepare.asm
@@ -6,4 +6,12 @@ init:
 late_game_routines = $01DF78+3+2+3+3
 
 end:
-    jml read3(late_game_routines)
+    pha 
+    lda.l late_game_routines
+    sta $00
+    lda.l late_game_routines+1
+    sta $01
+    lda.l late_game_routines+2
+    sta $02
+    pla 
+    jml [!dp]
